cae7be80ae2ed78c3243a7ed6a4eb0dec6e4f685
ethan.s.tran@student.uts.edu.au <ethan.s.tran@student.uts.edu.au>
2021-08-25 15:39:12 +1000

    80 percent complete

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    int i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i++;
+        Debug.Log(gameObject.name + ":" + i);
+    }
+}
