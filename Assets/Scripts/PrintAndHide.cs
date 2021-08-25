e483f7a6957ab03173d2b853516183e4d273b5dc
ethan.s.tran@student.uts.edu.au <ethan.s.tran@student.uts.edu.au>
2021-08-25 16:27:52 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -4,10 +4,13 @@ public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend;
     int i;
+    int rand;
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        rand = Random.Range(200, 251);
+        
     }
 
     // Update is called once per frame
@@ -15,5 +18,14 @@ public class PrintAndHide : MonoBehaviour
     {
         i++;
         Debug.Log(gameObject.name + ":" + i);
+
+        //deactive game object
+        if ( (gameObject.tag == "Red") && (i == 100) ) {
+            gameObject.SetActive(false);
+        }
+        //disable renderer component of game object 
+        if ( (gameObject.tag == "Blue") && (i == rand) ) {
+            rend.enabled = false;
+        }
     }
 }
